b090612e2710bcde58484991e6ee524e0956120e
zswang <wangjihu@sensorsdata.cn>
2018-02-27 14:47:23 +0800

    🐛 fixed: Input element is ineffective

--- src/base64.js
+++ src/base64.js
@@ -1,11 +1,16 @@
 (function () {
   function tryBd(str) {
     var result
+    var exists
     try {
       result = str.replace(/[a-z0-9+\/]+=*/ig, function (all) {
+        exists = true
         return decodeURIComponent(escape(atob(all)))
       })
     } catch (ex) {
+      return false
+    }
+    if (!exists) {
       return false
     }
     if (/^[a-zA-Z0-9]{1,3}$/.test(result)) {
@@ -53,11 +58,28 @@
     if (!editor && el.getAttribute && el.getAttribute('contenteditable')) {
       return
     }
-    if ((el instanceof HTMLInputElement && el.type === 'text') || el instanceof HTMLTextAreaElement) {
+    if (el.$history_value) {
+      el.value = el.$history_value
+      el.$history_value = null
+      return
+    }
+    if (el.$history_textContent) {
+      el.textContent = el.$history_textContent
+      el.$history_textContent = null
+      return
+    }
+    if (/input|textarea/i.test(el.tagName)) {
       if (editor) {
         if (tryBd(el.value) === false) {
+          el.$history_value = el.value
           el.value = be(el.value)
           fireInput(editor)
+        }
+      } else {
+        var r = tryBd(el.value)
+        if (r !== false) {
+          el.$history_value = el.value
+          el.value = r
         }
       }
       return
@@ -65,12 +87,14 @@
     if (el.nodeType === Node.TEXT_NODE) {
       if (editor) {
         if (tryBd(el.textContent) === false) {
+          el.$history_textContent = el.textContent
           el.textContent = be(el.textContent)
           fireInput(editor)
         }
       } else {
         var r = tryBd(el.textContent)
         if (r !== false) {
+          el.$history_textContent = el.textContent
           el.textContent = r
         }
       }
